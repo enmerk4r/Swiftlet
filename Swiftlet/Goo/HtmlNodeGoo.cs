ed309a1906cf62f6dcdbd77a45b5ee911c62be33
Sergey <p.i.g.a.c.h@gmail.com>
2022-07-24 14:56:02 -0400

    Html parsing WIP

---
+++ Swiftlet/Goo/HtmlNodeGoo.cs
@@ -0,0 +1,46 @@
+﻿using Grasshopper.Kernel.Types;
+using HtmlAgilityPack;
+using Swiftlet.DataModels.Enums;
+using Swiftlet.DataModels.Implementations;
+using Swiftlet.DataModels.Interfaces;
+using Swiftlet.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swiftlet.Goo
+{
+    public class HtmlNodeGoo : GH_Goo<HtmlNode>
+    {
+        public override bool IsValid => this.Value != null;
+
+        public override string TypeName => "Html Node";
+
+        public override string TypeDescription => "A queriable Html Node";
+
+        public HtmlNodeGoo()
+        {
+            this.Value = null;
+        }
+
+        public HtmlNodeGoo(HtmlNode node)
+        {
+            string html = node.OuterHtml;
+            HtmlDocument dom = new HtmlDocument();
+            dom.LoadHtml(html);
+            this.Value = dom.DocumentNode;
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return new HtmlNodeGoo(this.Value);
+        }
+
+        public override string ToString()
+        {
+            return this.Value.OuterHtml;
+        }
+    }
+}
