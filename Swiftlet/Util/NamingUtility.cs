0d2cc60f858945b4fc4e32331ebddaf2a872d876
enmerk4r <p.i.g.a.c.h@gmail.com>
2022-11-21 00:38:39 -0500

    Renamed Serve to Listen

--- Swiftlet/Util/NamingUtility.cs
+++ Swiftlet/Util/NamingUtility.cs
@@ -16,6 +16,6 @@ namespace Swiftlet.Util
         public const string READ_JSON = "4. Read JSON";
         public const string READ_HTML = "5. Read HTML";
         public const string SAVE_TO_DISK = "6. Save";
-        public const string SERVE = "7. Serve";
+        public const string LISTEN = "7. Listen";
     }
 }
