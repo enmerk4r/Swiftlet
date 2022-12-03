48a543ca0d02af7efb74876ebef2a3d4caa44191
Alex Schiftner <alex@shapediver.com>
2022-12-03 19:06:52 +0100

    CIDR block blacklist checker

--- Swiftlet/Util/IpBlacklistUtil.cs
+++ Swiftlet/Util/IpBlacklistUtil.cs
@@ -18,10 +18,13 @@ namespace Swiftlet.Util
                 address = address.MapToIPv4();
             }
 
-            // TODO replace this dummy check
-            if (address.GetAddressBytes()[0] == 192)
-                return true;
-
+            for (int i = 0; i < BlackListedIpBlocks.Length; i++)
+            {
+                if (BlackListedIpBlocks[i].Match(address.GetAddressBytes())) {
+                    return true;
+                }
+            }
+          
             return false;
         }
 
@@ -32,9 +35,126 @@ namespace Swiftlet.Util
             return false;
         }
 
+        /// <summary>
+        /// Loads blacklist of CIDR blocks from environment variable "BLOCKED_SUBNETS". 
+        /// Example blacklist: "192.168.0.0/24;192.168.128.0/17"
+        /// Example invalid blacklist (last item fails): "192.168.0.0/24;192.168.128.0/17;192.168.128.0/16"
+        /// </summary>
         public void LoadBlacklist()
         {
-            // TODO load blacklist
+            List<IpBlock> ipBlocks = new List<IpBlock>();
+
+            var cidrBlocksToBlock = Environment.GetEnvironmentVariable("BLOCKED_SUBNETS");
+            if (!String.IsNullOrEmpty(cidrBlocksToBlock))
+            {
+                var cidrBlocks = cidrBlocksToBlock.Split(';');
+                try
+                {
+                    for (int i = 0; i < cidrBlocks.Length; i++)
+                    {
+                        ipBlocks.Add(new IpBlock(cidrBlocks[i]));
+                    }
+                }
+                catch
+                {
+                    // a failure in parsing is suspicious, block everything
+                    ipBlocks.Add(new IpBlock("0.0.0.0/0"));
+                }
+            }
+
+            BlackListedIpBlocks = ipBlocks.ToArray();
         }
+
+        IpBlock[] BlackListedIpBlocks { get; set; }
+
+        struct ByteMask
+        {
+            public ByteMask(byte value, byte shift)
+            {
+                Value = value;
+                Shift = shift;
+            }
+
+            public byte Value { get; private set; }
+            public byte Shift { get; private set; }
+
+            public bool Match(byte b)
+            {
+                return (b >> Shift) == (Value >> Shift);
+            }
+        }
+
+        class IpBlock
+        {
+            public IpBlock(string cidrBlock)
+            {
+                var cidrBlockParts = cidrBlock.Split('/');
+                if (cidrBlockParts.Length != 2)
+                {
+                    throw new ArgumentException("Expected a CIDR block like X.Y.W.Z/B.");
+                }
+
+                if (!byte.TryParse(cidrBlockParts[1], out byte numBits))
+                {
+                    throw new ArgumentException("Expected a CIDR block like X.Y.W.Z/B where B is a number between 0 and 32.");
+                }
+
+                if (numBits < 0 || numBits > 32)
+                {
+                    throw new ArgumentException("Expected a CIDR block like X.Y.W.Z/B where B is a number between 0 and 32.");
+                }
+
+                var addressParts = cidrBlockParts[0].Split('.');
+                if (addressParts.Length != 4)
+                {
+                    throw new ArgumentException("Expected a CIDR block like X.Y.W.Z/B.");
+                }
+
+                byte numBitsShifted = numBits;
+
+                List<ByteMask> byteMasks = new List<ByteMask>(4);
+                for (int i = 0; i < 4; i++)
+                {
+                    if (!byte.TryParse(addressParts[i], out byte addressByte))
+                    {
+                        throw new ArgumentException("Expected a CIDR block like X.Y.W.Z/B where X, Y, W, Z are numbers between 0 and 255.");
+                    }
+
+                    byte shift = numBitsShifted >= 8 ? (byte)0 : (byte)(8 - numBitsShifted);
+                    numBitsShifted = (byte)Math.Max(0, numBitsShifted - 8);
+
+                    if ((255 & (addressByte << (8 - shift))) != 0)
+                    {
+                        throw new ArgumentException($"Not a valid CIDR block, netmask does not match address (all bits from bit {numBits} must be 0).");
+                    }
+
+                    byteMasks.Add(new ByteMask(addressByte, shift));
+                }
+
+                ByteMasks = byteMasks.ToArray();
+            }
+
+            public ByteMask[] ByteMasks { get; private set; }
+
+            public bool Match(byte[] addressBytes)
+            {
+                if (addressBytes.Length != 4)
+                {
+                    throw new ArgumentException("Expected a byte representation of an IPV4 address.");
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (!ByteMasks[i].Match(addressBytes[i]))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+
     }
 }
