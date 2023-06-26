788b1ddad8145e36071a64194af00505f497c6e3
enmerk4r <p.i.g.a.c.h@gmail.com>
2023-06-26 11:09:57 -0700

    Throttle component

---
+++ Swiftlet/Components/3_Send/Throttle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Swiftlet.DataModels.Implementations;
+using Swiftlet.Goo;
+using Swiftlet.Params;
+using Swiftlet.Util;
+
+namespace Swiftlet.Components
+{
+    public class Throttle : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the DeconstructHttpResponse class.
+        /// </summary>
+        public Throttle()
+          : base("Throttle", "THRTL",
+              "This is a simple passthrough component that lets you add a delay. "+
+              "Useful when sending multiple calls to APIs with limitations on maximum number of requests per minute",
+              NamingUtility.CATEGORY, NamingUtility.SEND)
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary;
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Input Data", "I", "Data to pass through", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Delay", "D", "Delay in milliseconds", GH_ParamAccess.item, 0);
+
+            pManager[0].Optional = true;
+            pManager[1].Optional = true;
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Output Data", "O", "Data that passed through unchanged", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            object obj = null;
+            int delay = 0;
+
+            DA.GetData(0, ref obj);
+            DA.GetData(1, ref delay);
+
+            Thread.Sleep(Math.Abs(delay));
+            
+            DA.SetData(0, obj);
+        }
+
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("6f4d4fde-70f7-4393-92dd-5d87fc94e1dc"); }
+        }
+    }
+}
