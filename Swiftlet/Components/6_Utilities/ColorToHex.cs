337b35af6a7181e697134bf6c9ad72b2a6823a56
enmerk4r <p.i.g.a.c.h@gmail.com>
2023-06-26 10:01:29 -0700

    Added Color to Hex and Hex to Color components

---
+++ Swiftlet/Components/6_Utilities/ColorToHex.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Swiftlet.Goo;
+using Swiftlet.Params;
+using Swiftlet.Util;
+
+namespace Swiftlet.Components
+{
+    public class ColorToHex : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the TextToByteArray class.
+        /// </summary>
+        public ColorToHex()
+          : base("Color to Hex", "CTH",
+              "Converts a Color value to a hex code",
+              NamingUtility.CATEGORY, NamingUtility.UTILITIES)
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.quinary;
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddColourParameter("Color", "C", "Color to convert", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Hex", "H", "Hex code", GH_ParamAccess.item);
+
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Color color = default(Color);
+
+            if (!DA.GetData(0, ref color)) return;
+
+            string hex = "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+
+            DA.SetData(0, hex);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("2e6f37c8-5c0c-4e70-9084-dcb7705871f2"); }
+        }
+    }
+}
---
+++ Swiftlet/Components/6_Utilities/HexToColor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Swiftlet.Goo;
+using Swiftlet.Params;
+using Swiftlet.Util;
+
+namespace Swiftlet.Components
+{
+    public class HexToColor : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the TextToByteArray class.
+        /// </summary>
+        public HexToColor()
+          : base("Hex to Color", "HTC",
+              "Parses a hex code into a color value",
+              NamingUtility.CATEGORY, NamingUtility.UTILITIES)
+        {
+        }
+
+        public override GH_Exposure Exposure => GH_Exposure.quinary;
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Hex", "H", "Hex color code", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddColourParameter("Color", "C", "Color represented by the hex code", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string hex = string.Empty;
+
+            DA.GetData(0, ref hex);
+
+            if (string.IsNullOrEmpty(hex)) return;
+
+            try
+            {
+                if (!hex.StartsWith("#"))
+                {
+                    hex = $"#{hex}";
+                }
+                Color color = ColorTranslator.FromHtml(hex);
+                DA.SetData(0, color);
+            }
+            catch
+            {
+                this.AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, $"Unable to parse the hex code: {hex}");
+                DA.SetData(0, null);
+            }
+
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("980e2b6e3f594d1c831b77446e2dc407"); }
+        }
+    }
+}
