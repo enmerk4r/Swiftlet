c5aa56d2d1d3a5c963f58078706423b20cb056b0
enmerk4r <p.i.g.a.c.h@gmail.com>
2023-07-15 19:36:44 -0400

    Base64 to and from byte array components

--- Swiftlet/Components/6_Utilities/Base64ToByteArray.cs
+++ Swiftlet/Components/6_Utilities/Base64ToByteArray.cs
@@ -9,14 +9,14 @@ using Swiftlet.Util;
 
 namespace Swiftlet.Components
 {
-    public class Base64ToText : GH_Component
+    public class Base64ToByteArray : GH_Component
     {
         /// <summary>
         /// Initializes a new instance of the TextToByteArray class.
         /// </summary>
-        public Base64ToText()
-          : base("Base64 to Text", "B64T",
-              "Converts a Base64 encoded string to cleartext",
+        public Base64ToByteArray()
+          : base("Base64 to Byte Array", "B64BA",
+              "Converts a Base64 encoded string to a byte array",
               NamingUtility.CATEGORY, NamingUtility.UTILITIES)
         {
         }
@@ -29,9 +29,6 @@ namespace Swiftlet.Components
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddTextParameter("Base64", "B", "Base64 encoded string", GH_ParamAccess.item);
-            pManager.AddTextParameter("Encoding", "E", "Can be ASCII, Unicode, UTF8, UTF7, UTF32", GH_ParamAccess.item, "UTF8");
-            
-            pManager[1].Optional = false;
         }
 
         /// <summary>
@@ -39,7 +36,7 @@ namespace Swiftlet.Components
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddTextParameter("Text", "T", "Converted Text", GH_ParamAccess.item);
+            pManager.AddParameter(new ByteArrayParam(), "Byte Array", "A", "Output Byte Array", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -49,33 +46,10 @@ namespace Swiftlet.Components
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             string txt = string.Empty;
-            string encoding = string.Empty;
-
             DA.GetData(0, ref txt);
-            DA.GetData(1, ref encoding);
 
-            string outputString = string.Empty;
             byte[] base64DecodedBytes = System.Convert.FromBase64String(txt);
-
-            string upperEncoding = encoding.ToUpper();
-
-            switch (upperEncoding)
-            {
-                case "ASCII":
-                    outputString = Encoding.ASCII.GetString(base64DecodedBytes); break;
-                case "UNICODE":
-                    outputString = Encoding.Unicode.GetString(base64DecodedBytes); break;
-                case "UTF8":
-                    outputString = Encoding.UTF8.GetString(base64DecodedBytes); break;
-                case "UTF7":
-                    outputString = Encoding.UTF7.GetString(base64DecodedBytes); break;
-                case "UTF32":
-                    outputString = Encoding.UTF32.GetString(base64DecodedBytes); break;
-                default:
-                    this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"{encoding} is an unknown encoding"); return;
-            }
-
-            DA.SetData(0, outputString);
+            DA.SetData(0, new ByteArrayGoo(base64DecodedBytes));
         }
 
         /// <summary>
@@ -87,7 +61,7 @@ namespace Swiftlet.Components
             {
                 //You can add image files to your project resources and access them like this:
                 // return Resources.IconForThisComponent;
-                return Properties.Resources.Icons_base64_to_text_24x24;
+                return null;
             }
         }
 
@@ -96,7 +70,7 @@ namespace Swiftlet.Components
         /// </summary>
         public override Guid ComponentGuid
         {
-            get { return new Guid("2385c560-3f23-4196-bb17-5a0c8f7b4aca"); }
+            get { return new Guid("792af514-90fe-45b0-82d1-2bb90c55e813"); }
         }
     }
 }
