dcef56c37e601df9b62f18b198886d949083c9a1
enmerk4r <p.i.g.a.c.h@gmail.com>
2023-07-15 16:43:25 -0400

    Separating Bitmap into a separate GH type

--- Swiftlet/Components/6_Utilities/ByteArrayToBitmap.cs
+++ Swiftlet/Components/6_Utilities/ByteArrayToBitmap.cs
@@ -25,7 +25,7 @@ namespace Swiftlet.Components
         {
         }
 
-        public override GH_Exposure Exposure => GH_Exposure.quarternary;
+        public override GH_Exposure Exposure => GH_Exposure.septenary;
 
         /// <summary>
         /// Registers all the input parameters for this component.
@@ -33,10 +33,6 @@ namespace Swiftlet.Components
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddParameter(new ByteArrayParam(), "Byte Array", "A", "Input Byte Array", GH_ParamAccess.item);
-            pManager.AddRectangleParameter("Rectangle", "R", "Optional boundary", GH_ParamAccess.item);
-
-            pManager[1].Optional = true;
-
         }
 
         /// <summary>
@@ -44,10 +40,7 @@ namespace Swiftlet.Components
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddMeshParameter("Bitmap", "B", "Output bitmap", GH_ParamAccess.item);
-            pManager.AddColourParameter("Colors", "C", "Bitmap colors (in the original order)", GH_ParamAccess.list);
-            pManager.AddIntegerParameter("Width", "W", "Bitmap width (in pixels)", GH_ParamAccess.item);
-            pManager.AddIntegerParameter("Height", "H", "Bitmap height (in pixels)", GH_ParamAccess.item);
+            pManager.AddParameter(new BitmapParam(),"Bitmap", "B", "Output bitmap", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -57,8 +50,6 @@ namespace Swiftlet.Components
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             ByteArrayGoo goo = null;
-            Rectangle3d rect = default(Rectangle3d);
-
             DA.GetData(0, ref goo);
 
             Bitmap bmp;
@@ -67,48 +58,8 @@ namespace Swiftlet.Components
                 bmp = new Bitmap(ms);
             }
 
-            
-            Mesh mesh;
-
-            if (DA.GetData(1, ref rect))
-            {
-                mesh = Mesh.CreateFromPlane(rect.Plane, rect.X, rect.Y, bmp.Width - 1, bmp.Height - 1);
-            }
-            else
-            {
-                Interval xInt = new Interval(0, bmp.Width);
-                Interval yInt = new Interval(0, bmp.Height);
-
-                mesh = Mesh.CreateFromPlane(Plane.WorldXY, xInt, yInt, bmp.Width - 1, bmp.Height - 1);
-            }
-
-            List<List<Color>> rows = new List<List<Color>>();
-
-            for (int y = bmp.Height - 1; y >= 0 ; y--)
-            {
-                List<Color> row = new List<Color>();
-                for (int x = 0; x < bmp.Width; x++)
-                {
-                    Color c = bmp.GetPixel(x, y);
-                    mesh.VertexColors.Add(c);
-                    row.Add(c);
-                }
-                rows.Add(row);
-            }
-
-            List<Color> colors = new List<Color>();
-            rows.Reverse();
-
-            foreach (List<Color> row in rows)
-            {
-                colors.AddRange(row);
-            }
-
-
-            DA.SetData(0, mesh);
-            DA.SetDataList(1, colors);
-            DA.SetData(2, bmp.Width);
-            DA.SetData(3, bmp.Height);
+            BitmapGoo output = new BitmapGoo(bmp);
+            DA.SetData(0, output);
         }
 
         /// <summary>
