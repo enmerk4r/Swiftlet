db14c7f54df8846a0a8f82237f941adaab19c6cc
enmerk4r <p.i.g.a.c.h@gmail.com>
2023-07-16 00:37:37 -0400

    Icons

--- Swiftlet/Components/6_Utilities/ColorToHex.cs
+++ Swiftlet/Components/6_Utilities/ColorToHex.cs
@@ -66,7 +66,7 @@ namespace Swiftlet.Components
             {
                 //You can add image files to your project resources and access them like this:
                 // return Resources.IconForThisComponent;
-                return null;
+                return Properties.Resources.Icons_color_to_hex_24x24;
             }
         }
 
