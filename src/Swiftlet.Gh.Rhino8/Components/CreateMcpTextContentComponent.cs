0d64216743a1a8f23e4e6ad6b06ed25fcde50b28
Sergey Pigach <p.i.g.a.c.h@gmail.com>
2026-04-20 23:13:13 -0400

    Update CreateMcpTextContentComponent.cs

--- src/Swiftlet.Gh.Rhino8/Components/CreateMcpTextContentComponent.cs
+++ src/Swiftlet.Gh.Rhino8/Components/CreateMcpTextContentComponent.cs
@@ -1,4 +1,6 @@
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
 using Swiftlet.Core.Mcp;
 using Swiftlet.Gh.Rhino8.Goo;
 using Swiftlet.Gh.Rhino8.Params;
@@ -26,6 +28,11 @@ public sealed class CreateMcpTextContentComponent : GH_Component
 
     protected override void SolveInstance(IGH_DataAccess DA)
     {
+        if (HasJsonInput())
+        {
+            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "You might want to use Stringify JSON first :)");
+        }
+
         string text = string.Empty;
         if (!DA.GetData(0, ref text))
         {
@@ -33,6 +40,39 @@ public sealed class CreateMcpTextContentComponent : GH_Component
         }
 
         DA.SetData(0, new McpContentBlockGoo(new McpTextContentBlock(text)));
+    }
+
+    private bool HasJsonInput()
+    {
+        IGH_Param inputParam = Params.Input[0];
+        foreach (IGH_Param source in inputParam.Sources)
+        {
+            foreach (IGH_Goo goo in EnumerateData(source))
+            {
+                if (goo is JsonNodeGoo or JsonObjectGoo or JsonArrayGoo or JsonValueGoo)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static IEnumerable<IGH_Goo> EnumerateData(IGH_Param param)
+    {
+        IGH_Structure structure = param.VolatileData;
+        for (int pathIndex = 0; pathIndex < structure.PathCount; pathIndex++)
+        {
+            var branch = structure.get_Branch(pathIndex);
+            foreach (object? item in branch)
+            {
+                if (item is IGH_Goo goo)
+                {
+                    yield return goo;
+                }
+            }
+        }
     }
 
     protected override System.Drawing.Bitmap? Icon => ShellIcons.For(GetType());
